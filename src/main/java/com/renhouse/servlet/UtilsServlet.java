1b37970df5b11c30689ff13f4741d6b417f903a9
Full_Stack <1078433422@qq.com>
2020-07-17 15:00:44 +0800

    发送邮件Servlet配置完成

---
+++ src/main/java/com/renhouse/servlet/UtilsServlet.java
@@ -0,0 +1,40 @@
+package com.renhouse.servlet;
+
+import com.renhouse.utils.EmailUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class UtilsServlet extends BaseServlet {
+    /**
+     * 发送邮件的方法
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void sendEmail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String Subject = "房东消息";
+        String SendMessage = "您的房屋即将到期，请及时联系房东续租，或处理退房手续";
+        EmailUtils emailUtils = new EmailUtils("1078433422@qq.com",Subject,SendMessage,"1078433422@qq.com","mixhkqoqgsziiaib");
+        try {
+            emailUtils.Send();
+            String result = "{" +
+                    "  \"code\": 0," +
+                    "  \"msg\": " + "\"发送成功！\"" +
+                    "} ";
+            response.getWriter().write(result);
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+            String result = "{" +
+                    "  \"code\": 1," +
+                    "  \"msg\": " + "\"发送失败！\"" +
+                    "} ";
+            response.getWriter().write(result);
+        }
+    }
+}
