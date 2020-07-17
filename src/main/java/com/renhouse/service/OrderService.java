3b818ea86e661b519b65871938d9a9c7b7c246e6
HolaAsuka <1322040529@qq.com>
2020-07-17 10:37:18 +0800

    Update notes in service

--- src/main/java/com/renhouse/service/OrderService.java
+++ src/main/java/com/renhouse/service/OrderService.java
@@ -11,9 +11,25 @@ public interface OrderService {
      */
     public void deleteOrderById(Integer id);
 
+    /**
+     * 根据id查订单
+     * @param id
+     * @return
+     */
     public Order queryOrderById(Integer id);
 
+    /**
+     * 更新订单
+     * @param order
+     */
     public void updateOrder(Order order);
 
+    /**
+     * 得到订单的分页信息
+     * @param username
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
     Page<Order> page(String username, int pageNo, int pageSize);
 }
