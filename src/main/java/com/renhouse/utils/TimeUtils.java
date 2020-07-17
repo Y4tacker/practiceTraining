4162395c6915a0d6f82891e7a4ed4c21d5d123f4
1treeforest <humphreywcl@gmail.com>
2020-07-17 13:27:30 +0800

    完成了时间工具类和测试，全部通过。供调用的方法传参均为String

--- src/main/java/com/renhouse/utils/TimeUtils.java
+++ src/main/java/com/renhouse/utils/TimeUtils.java
@@ -1,6 +1,9 @@
 package com.renhouse.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * @ClassName TimeUtils
@@ -11,29 +14,58 @@ import java.text.SimpleDateFormat;
  */
 public class TimeUtils {
     /**
-     * 格式化日期显示格式
+     * 取得给定字符串的日期对象.
      *
-     * @param sdate
-     *            原始日期格式 s - 表示 "yyyy-MM-dd" 形式的日期的 String 对象
-     * @param format
-     *            格式化后日期格式
-     * @return 格式化后的日期显示
+     * @param dateStr 日期描述 从给定字符串的开始分析文本，以生成一个日期对象
+     * @return 给定字符串描述的日期对象。
      */
-    public static String dateFormat(String sdate, String format) {
-        SimpleDateFormat formatter = new SimpleDateFormat(format);
-        java.sql.Date date = java.sql.Date.valueOf(sdate);
-        String dateString = formatter.format(date);
+    public static Date getDateFromString(String dateStr) {
+        String format = "yyyy-MM-dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        Date resDate = null;
+        try {
+            resDate = sdf.parse(dateStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resDate;
+    }
 
-        return dateString;
+    /**
+     * 根据给定的格式与时间(Date类型的)，返回时间字符串。
+     *
+     * @param date 指定的日期
+     * @return String 指定格式的时间字符串.
+     */
+    public static String getFormatDate(Date date) {
+        String format = "yyyy-MM-dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(date);
+    }
+
+    /**
+     * 判断一个日期是否在开始日期和结束日期之间。
+     *
+     * @param srcDate   目标日期 yyyy/MM/dd 或者 yyyy-MM-dd
+     * @param startDate 开始日期 yyyy/MM/dd 或者 yyyy-MM-dd
+     * @param endDate   结束日期 yyyy/MM/dd 或者 yyyy-MM-dd
+     * @return 大于等于开始日期小于等于结束日期，那么返回true，否则返回false
+     */
+    public static boolean isInStartEnd(String srcDate, String startDate,
+                                       String endDate) {
+        if (startDate.compareTo(srcDate) <= 0
+                && endDate.compareTo(srcDate) >= 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
      * 起始日期yyyy-MM-dd与终止日期yyyy-MM-dd之间跨度的月数。
      *
-     * @param beginDate
-     *            格式为yyyy-MM-dd
-     * @param beginDate
-     *            格式为yyyy-MM-dd
+     * @param beginDate 格式为yyyy-MM-dd
+     * @param beginDate 格式为yyyy-MM-dd
      * @return 相差月整数。
      */
     public static int getInterval(String beginDate, String endDate) {
@@ -41,7 +73,7 @@ public class TimeUtils {
         String regex = "-";// 按照-来进行切割
 
         String[] date1 = beginDate.split(regex);
-        String[] date2 = beginDate.split(regex);
+        String[] date2 = endDate.split(regex);
 
         int y1 = Integer.parseInt(date1[0]);
         int y2 = Integer.parseInt(date2[0]);
@@ -50,6 +82,66 @@ public class TimeUtils {
         int m2 = Integer.parseInt(date2[1]);
 
         return ((y2 - y1) * 12)
-                + (m2 - m1) + 1; //相差的月数
+                + (m2 - m1); //相差的月数
+    }
+
+    /**
+     * 取得给定日期加上一定天数后的格式化日期字符串.
+     *
+     * @param dateStr   给定的日期对象
+     * @param amount 需要添加的天数，如果是向前的天数，使用负数就可以，已根据需求设置为30天
+     * @return String 加上一定天数以后的日期字符串.
+     */
+    public static String getDateAdd(String dateStr, int amount) {
+        Calendar cal = new GregorianCalendar();
+        Date date=getDateFromString(dateStr);
+        cal.setTime(date);
+        cal.add(GregorianCalendar.DATE, amount);
+        return getFormatDate(cal.getTime());
+    }
+
+    /**
+     * 获得当前日期的格式化字符串
+     *
+     * @return String 系统当前时间格式化字符串
+     */
+    public static String getCurrentDateStr() {
+        return getFormatDate(new Date());
+    }
+
+    /**
+     * 获得当前日期的对象
+     *
+     * @return Date 系统当前时间对象
+     */
+    public static Date getCurrentDate() {
+        return new Date();
+    }
+
+    /**
+     * 获得当前日期固定间隔天数的日期，如前60天dateAdd(-60)
+     *
+     * @param interval 距今天的间隔日期长度，向前为负，向后为正
+     * @return java.lang.String 按照格式输出的间隔的日期字符串.
+     */
+    public static String getFormatCurrentAdd(int interval) {
+        String d = getDateAdd(getFormatDate(new Date()), interval);
+
+        return d;
+    }
+
+    /**
+     * 取得给定日期加上一定天数后的日期对象.
+     *
+     * @param dateStr   给定的日期对象
+     * @param amount 需要添加的天数，如果是向前的天数，使用负数就可以.
+     * @return Date 加上一定天数以后的Date对象.
+     */
+    public static String getFormatDateAdd(String dateStr, int amount) {
+        Calendar cal = new GregorianCalendar();
+        Date date=getDateFromString(dateStr);
+        cal.setTime(date);
+        cal.add(GregorianCalendar.DATE, amount);
+        return getFormatDate(cal.getTime());
     }
 }
