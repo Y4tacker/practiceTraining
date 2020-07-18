405c420c9989f9fc975ddc51f66a536cd1b744dc
Full_Stack <1078433422@qq.com>
2020-07-18 10:36:24 +0800

    Update notice.js

--- target/practicalTraining/static/script/notice.js
+++ target/practicalTraining/static/script/notice.js
@@ -12,7 +12,10 @@ function showNotice(){
             var btn = layero.find('.layui-layer-btn');
             btn.css('text-align', 'center');
             btn.on("click",function(){
-
+                layer.tips('谢谢配合！', 'body > div.layui-layout.layui-layout-admin > div.layui-header > ul > li.layui-nav-item.layui-this > a', {
+                    tips: 3,
+                    time : 1000
+                });
             });
         },
         cancel: function(index, layero){
